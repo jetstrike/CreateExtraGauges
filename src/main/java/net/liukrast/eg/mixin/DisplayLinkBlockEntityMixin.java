98ae10377e9f28f13246b935e152d46c8e5d1f9a
DMills <jetstrike3000@gmail.com>
2026-07-11 04:16:56 -0500

    Redirect Level.isLoaded in DisplayLinkBlockEntity.updateGatheredData to query all server levels for collector sources

--- src/main/java/net/liukrast/eg/mixin/DisplayLinkBlockEntityMixin.java
+++ src/main/java/net/liukrast/eg/mixin/DisplayLinkBlockEntityMixin.java
@@ -2,6 +2,8 @@ package net.liukrast.eg.mixin;
 
 import com.simibubi.create.content.redstone.displayLink.DisplayLinkBlockEntity;
 import net.liukrast.eg.content.logistics.link.DisplayCollectorBlockEntity;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -20,5 +22,27 @@ public class DisplayLinkBlockEntityMixin {
             return true;
         }
         return list.contains(o);
+    }
+
+    @Redirect(
+        method = "updateGatheredData",
+        at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;isLoaded(Lnet/minecraft/core/BlockPos;)Z")
+    )
+    private boolean redirectIsLoaded(Level level, BlockPos pos) {
+        if ((Object) this instanceof DisplayCollectorBlockEntity dc) {
+            if (pos.equals(dc.getTargetPosition())) {
+                return level.isLoaded(pos);
+            }
+            var server = level.getServer();
+            if (server != null) {
+                for (var serverLevel : server.getAllLevels()) {
+                    if (serverLevel.isLoaded(pos)) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+        return level.isLoaded(pos);
     }
 }
