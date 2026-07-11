04dba2b836f669b2c056e4fa2b3084ac98352431
DMills <jetstrike3000@gmail.com>
2026-07-11 07:02:12 -0500

    Refactor behavior mixins to use Redirect to avoid collisions with Deployer's mixins. Bump mod version to 1.0.15

--- src/main/java/net/liukrast/eg/mixin/AbstractPanelSupportBehaviourMixin.java
+++ src/main/java/net/liukrast/eg/mixin/AbstractPanelSupportBehaviourMixin.java
@@ -1,59 +1,28 @@
 package net.liukrast.eg.mixin;
 
 import com.simibubi.create.AllBlocks;
-import com.simibubi.create.content.logistics.factoryBoard.FactoryPanelBehaviour;
 import com.simibubi.create.content.logistics.factoryBoard.FactoryPanelPosition;
-import net.liukrast.deployer.lib.logistics.board.AbstractPanelBehaviour;
 import net.liukrast.deployer.lib.logistics.board.connection.AbstractPanelSupportBehaviour;
-import net.liukrast.deployer.lib.logistics.board.connection.PanelConnection;
-import net.liukrast.deployer.lib.logistics.board.connection.ProvidesConnection;
-import net.liukrast.deployer.lib.mixin.FactoryPanelSupportAccessor;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
-import org.jetbrains.annotations.Nullable;
 
 @Mixin(value = AbstractPanelSupportBehaviour.class, remap = false)
-public abstract class AbstractPanelSupportBehaviourMixin extends AbstractPanelSupportBehaviour {
+public abstract class AbstractPanelSupportBehaviourMixin {
 
-    public AbstractPanelSupportBehaviourMixin() {
-        super(null, null, null);
-    }
-
-    /**
-     * @author Antigravity
-     * @reason Prevent connection deletion during chunk loading / async world initialization.
-     */
-    @Overwrite
-    public @Nullable <T> List<AbstractPanelBehaviour.ConnectionValue<T>> getAllValuesWithSource(PanelConnection<T> connection) {
-        List<AbstractPanelBehaviour.ConnectionValue<T>> out = new ArrayList<>();
-        for (Iterator<FactoryPanelPosition> iterator = getLinkedPanels().iterator(); iterator.hasNext(); ) {
-            FactoryPanelPosition panelPos = iterator.next();
-            if (!getWorld().isLoaded(panelPos.pos()))
-                return null;
-            FactoryPanelBehaviour behaviour = FactoryPanelBehaviour.at(getWorld(), panelPos);
-            if (behaviour == null) {
-                BlockState state = getWorld().getBlockState(panelPos.pos());
-                if (AllBlocks.FACTORY_GAUGE.has(state)) {
-                    continue;
-                }
-                iterator.remove();
-                ((FactoryPanelSupportAccessor)this).deployer$setChanged(true);
-                continue;
-            }
-            if (!behaviour.isActive())
-                continue;
-            var conn = behaviour.targetedByLinks.get(getPos());
-            if(conn == null) continue;
-            var pc = ProvidesConnection.getCurrentConnection(conn, () -> ProvidesConnection.getPossibleConnections(behaviour, this).stream().findFirst().orElse(null));
-            if(pc == null || pc != connection) continue;
-            var opt = ((ProvidesConnection)behaviour).getConnectionValue(connection);
-            opt.ifPresent(t -> out.add(new AbstractPanelBehaviour.ConnectionValue<>(conn, t)));
+    @Redirect(
+        method = "getAllValuesWithSource",
+        at = @At(value = "INVOKE", target = "Ljava/util/Iterator;remove()V")
+    )
+    private void redirectRemoveAllValues(Iterator<FactoryPanelPosition> iterator, @Local FactoryPanelPosition panelPos) {
+        BlockState state = ((AbstractPanelSupportBehaviour) (Object) this).getWorld().getBlockState(panelPos.pos());
+        if (AllBlocks.FACTORY_GAUGE.has(state)) {
+            return;
         }
-        return out;
+        iterator.remove();
     }
 }
--- src/main/java/net/liukrast/eg/mixin/FactoryPanelSupportBehaviourMixin.java
+++ src/main/java/net/liukrast/eg/mixin/FactoryPanelSupportBehaviourMixin.java
@@ -1,82 +1,40 @@
 package net.liukrast.eg.mixin;
 
 import com.simibubi.create.AllBlocks;
-import com.simibubi.create.content.logistics.factoryBoard.FactoryPanelBehaviour;
 import com.simibubi.create.content.logistics.factoryBoard.FactoryPanelPosition;
 import com.simibubi.create.content.logistics.factoryBoard.FactoryPanelSupportBehaviour;
-import com.simibubi.create.foundation.blockEntity.behaviour.BlockEntityBehaviour;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.util.Iterator;
-import java.util.List;
-import org.jetbrains.annotations.Nullable;
 
 @Mixin(value = FactoryPanelSupportBehaviour.class, remap = false)
-public abstract class FactoryPanelSupportBehaviourMixin extends BlockEntityBehaviour {
+public abstract class FactoryPanelSupportBehaviourMixin {
 
-    @Shadow
-    private List<FactoryPanelPosition> linkedPanels;
-
-    @Shadow
-    private boolean changed;
-
-    public FactoryPanelSupportBehaviourMixin() {
-        super(null);
-    }
-
-    /**
-     * @author Antigravity
-     * @reason Prevent connection deletion during chunk loading / async world initialization.
-     */
-    @Overwrite
-    public void notifyPanels() {
-        if (getWorld().isClientSide())
+    @Redirect(
+        method = "notifyPanels",
+        at = @At(value = "INVOKE", target = "Ljava/util/Iterator;remove()V")
+    )
+    private void redirectRemoveNotify(Iterator<FactoryPanelPosition> iterator, @Local FactoryPanelPosition panelPos) {
+        BlockState state = ((FactoryPanelSupportBehaviour) (Object) this).getWorld().getBlockState(panelPos.pos());
+        if (AllBlocks.FACTORY_GAUGE.has(state)) {
             return;
-        for (Iterator<FactoryPanelPosition> iterator = linkedPanels.iterator(); iterator.hasNext(); ) {
-            FactoryPanelPosition panelPos = iterator.next();
-            if (!getWorld().isLoaded(panelPos.pos()))
-                continue;
-            FactoryPanelBehaviour behaviour = FactoryPanelBehaviour.at(getWorld(), panelPos);
-            if (behaviour == null) {
-                BlockState state = getWorld().getBlockState(panelPos.pos());
-                if (AllBlocks.FACTORY_GAUGE.has(state)) {
-                    continue;
-                }
-                iterator.remove();
-                changed = true;
-                continue;
-            }
-            behaviour.checkForRedstoneInput();
         }
+        iterator.remove();
     }
 
-    /**
-     * @author Antigravity
-     * @reason Prevent connection deletion during chunk loading / async world initialization.
-     */
-    @Overwrite
-    @Nullable
-    public Boolean shouldBePoweredTristate() {
-        for (Iterator<FactoryPanelPosition> iterator = linkedPanels.iterator(); iterator.hasNext(); ) {
-            FactoryPanelPosition panelPos = iterator.next();
-            if (!getWorld().isLoaded(panelPos.pos()))
-                return null;
-            FactoryPanelBehaviour behaviour = FactoryPanelBehaviour.at(getWorld(), panelPos);
-            if (behaviour == null) {
-                BlockState state = getWorld().getBlockState(panelPos.pos());
-                if (AllBlocks.FACTORY_GAUGE.has(state)) {
-                    continue;
-                }
-                iterator.remove();
-                changed = true;
-                continue;
-            }
-            if (behaviour.isActive() && behaviour.satisfied && behaviour.count != 0)
-                return true;
+    @Redirect(
+        method = "shouldBePoweredTristate",
+        at = @At(value = "INVOKE", target = "Ljava/util/Iterator;remove()V")
+    )
+    private void redirectRemoveTristate(Iterator<FactoryPanelPosition> iterator, @Local FactoryPanelPosition panelPos) {
+        BlockState state = ((FactoryPanelSupportBehaviour) (Object) this).getWorld().getBlockState(panelPos.pos());
+        if (AllBlocks.FACTORY_GAUGE.has(state)) {
+            return;
         }
-        return false;
+        iterator.remove();
     }
 }
